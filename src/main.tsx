6bcd0f7d01ad2cd9fbfd77990aaaeafa4fbea54c
nurlan <baitassovnurlan1507@gmail.com>
2024-10-22 02:07:23 +0500

    feat:google auth ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import {Provider} from "react-redux";
-import {store} from './store'
+import { Provider } from "react-redux";
+import { store } from './store'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './assets/base.css'
 
+const googleClientId = '229084646214-h4envqms90napi5k6os5r4u4us4f3j8o.apps.googleusercontent.com'
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <GoogleOAuthProvider clientId={googleClientId}>
+    <StrictMode>
       <Provider store={store}>
-          <App />
+        <App />
       </Provider>
-  </StrictMode>,
+    </StrictMode>
+  </GoogleOAuthProvider>
+  ,
 )
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,15 +1,23 @@
-import {FC} from "react";
-import {Formik, Form, Field} from "formik";
-import {registrationValidationSchema, initialValues, regions} from "./helpers/validation.ts";
+import { FC } from "react";
+import { Formik, Form, Field } from "formik";
+import { registrationValidationSchema, initialValues, regions } from "./helpers/validation.ts";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 import './styles.css'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import DataSelector from "../../components/Selector";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const SignUp: FC = () => {
 
+    const googleLogin = useGoogleLogin({
+        onSuccess: codeResponse => {
+            console.log('GOOOGLE');
+            console.log(codeResponse)
+        },
+        flow: 'auth-code',
+    });
 
     return (
         <section className={'w-full flex mb-16 mt-16'}>
@@ -28,15 +36,21 @@ const SignUp: FC = () => {
                         validateOnChange={false}
                         validationSchema={registrationValidationSchema}>
                         <Form>
-                            <Input name={'fullName'} icon={'fas fa-user'} placeholder={'Name'} margin={'mb-4'}/>
-                            <Input name={'email'} icon={'fas fa-envelope'} placeholder={'Email'} margin={'mb-4'}/>
+                            <Input name={'fullName'} icon={'fas fa-user'} placeholder={'Name'} margin={'mb-4'} />
+                            <Input name={'email'} icon={'fas fa-envelope'} placeholder={'Email'} margin={'mb-4'} />
                             <div className={'mb-4'}>
                                 <DataSelector name={'region'} data={regions as []}></DataSelector>
                             </div>
-                            <Input name={'password'} placeholder={'Password'} margin={'mb-4'} icon="fas fa-lock"/>
-                            <Input name={'confirmPassword'} placeholder={'Confirm Password'} margin={'mb-4'} icon="fas fa-lock"/>
-                            <Button className={'mb-2 text-white'} type={'submit'} text={'Create Account'}/>
-                            <Button type="button" text={"Sign Up with Google"} icon="fa-brands fa-google" className={'bg-transparent border-2 text-black mb-4'}/>
+                            <Input name={'password'} placeholder={'Password'} margin={'mb-4'} icon="fas fa-lock" />
+                            <Input name={'confirmPassword'} placeholder={'Confirm Password'} margin={'mb-4'} icon="fas fa-lock" />
+                            <Button className={'mb-2 text-white'} type={'submit'} text={'Create Account'} />
+                            <Button
+                                onClick={() => { googleLogin() }}
+                                type="button"
+                                text={"Sign Up with Google"}
+                                icon="fa-brands fa-google"
+                                className={'bg-transparent border-2 text-black mb-4'}
+                            />
                             <p className={'text-center'}>
                                 Already have account? &nbsp;&nbsp;
                                 <Link className={'inline underline'} to={'/'}>Log In</Link>
